5da337fd68cc57039bed885d131ef680abcbb102
rofl0r <retnyg@gmx.net>
2012-12-25 19:52:49 +0100

    add script to create tarball distribution

---
+++ create-dist.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+if [[ -z "$VER" ]] ; then
+	echo set VER!
+	exit
+fi
+me=`pwd`
+
+proj=ixchat
+projver=${proj}-${VER}
+repo=http://github.com/rofl0r/$proj
+repo=~/cdev/cdev/$proj
+
+tempdir=/tmp/${proj}-0000
+rm -rf $tempdir
+mkdir -p $tempdir
+
+cd $tempdir
+git clone $repo $projver
+rm -rf $projver/.git
+rm -rf $projver/docs
+
+tar cJf $proj.tar.xz $projver/
+mv $proj.tar.xz $me/$projver.tar.xz
