hexchat/hexchat

Respect use global flag with SASL

src/common/inbound.c
@@ -1744,11 +1744,14 @@ static const char *sasl_mechanisms[] =
 void
 inbound_sasl_authenticate (server *serv, char *data)
 {
+		ircnet *net = (ircnet*)serv->network;
 		char *user, *pass = NULL;
 		const char *mech = sasl_mechanisms[serv->sasl_mech];
 
-		user = (((ircnet*)serv->network)->user)
-				? (((ircnet*)serv->network)->user) : prefs.hex_irc_user_name;
+		if (net->user && !(net->flags & FLAG_USE_GLOBAL))
+			user = net->user;
+		else
+			user = prefs.hex_irc_user_name;
 
 		switch (serv->sasl_mech)
 		{

rofl0r/ixchat

src/common/inbound.c
@@ -1599,11 +1599,14 @@ static const char *sasl_mechanisms[] =
 void
 inbound_sasl_authenticate (server *serv, char *data)
 {
+		ircnet *net = (ircnet*)serv->network;
 		char *user, *pass = NULL;
 		const char *mech = sasl_mechanisms[serv->sasl_mech];
 
-		user = (((ircnet*)serv->network)->user)
-				? (((ircnet*)serv->network)->user) : prefs.username;
+		if (net->user && !(net->flags & FLAG_USE_GLOBAL))
+			user = net->user;
+		else
+			user = prefs.username;
 
 		switch (serv->sasl_mech)
 		{